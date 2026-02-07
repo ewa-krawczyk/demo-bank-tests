f98685934a21e0e157e37706f4f0fbdfa9f0d16b
Ewa Krawczyk <75725771+ewa-krawczyk@users.noreply.github.com>
2026-02-07 14:28:45 +0100

    first aaa implementation

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -4,12 +4,21 @@ import { test, expect } from '@playwright/test';
 test.describe('User login to Demobank', () => {
 
   test('successful login with correct credentials', async ({ page }) => {
-    await page.goto('https://demo-bank.vercel.app/');
-    await page.getByTestId('login-input').fill('tester12');
-    await page.getByTestId('password-input').fill('tester12');
+
+    // Arrange 
+    const url = 'https://demo-bank.vercel.app/';
+    const userName = 'tester12';
+    const userPassword = 'tester12';
+    const expectedUserName = "Jan Demobankowy";
+
+    // Act
+    await page.goto(url);
+    await page.getByTestId('login-input').fill(userName);
+    await page.getByTestId('password-input').fill(userPassword);
     await page.getByTestId('login-button').click();
 
-    await expect(page.getByTestId('user-name')).toHaveText("Jan Demobankowy");
+    // Assert
+    await expect(page.getByTestId('user-name')).toHaveText(expectedUserName);
   });
 
   test('unsuccessful login with too short username', async ({ page }) => {
@@ -20,7 +29,7 @@ test.describe('User login to Demobank', () => {
     await expect(page.getByTestId('error-login-id')).toHaveText("identyfikator ma min. 8 znaków");
   });
 
-    test('unsuccessful login with too short password', async ({ page }) => {
+  test('unsuccessful login with too short password', async ({ page }) => {
     await page.goto('https://demo-bank.vercel.app/');
     await page.getByTestId('login-input').fill('tester12');
     await page.getByTestId('password-input').fill('test');
--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -3,21 +3,34 @@ import { test, expect } from '@playwright/test';
 test.describe('Pulpit tests', () => {
     test.describe.configure({ retries: 3 });
     test('quick payment with correct data', async ({ page }) => {
-        await page.goto('https://demo-bank.vercel.app');
-        await page.getByTestId('login-input').fill('tester12');
-        await page.getByTestId('password-input').fill('password');
+        // Arrange 
+        const url = 'https://demo-bank.vercel.app/';
+        const userName = 'tester12';
+        const userPassword = 'tester12';
+
+        const receiverID = '2';
+        const transferAmount = '150';
+        const transferTitle = 'pizza';
+        const expectedTransferReceiver = 'Chuck Demobankowy';
+
+        // Act
+        await page.goto(url);
+        await page.getByTestId('login-input').fill(userName);
+        await page.getByTestId('password-input').fill(userPassword);
         await page.getByTestId('login-button').click();
 
-        await page.locator('#widget_1_transfer_receiver').selectOption('2');
-        await page.locator('#widget_1_transfer_amount').fill('150');
-        await page.locator('#widget_1_transfer_title').fill('pizza');
+        await page.locator('#widget_1_transfer_receiver').selectOption(receiverID);
+        await page.locator('#widget_1_transfer_amount').fill(transferAmount);
+        await page.locator('#widget_1_transfer_title').fill(transferTitle);
 
         await page.getByRole('button', { name: 'wykonaj' }).click();
         await page.getByTestId('close-button').click();
 
-        await expect(page.locator('#show_messages')).toHaveText('Przelew wykonany! Chuck Demobankowy - 150,00PLN - pizza');
+        // Assert
+        await expect(page.locator('#show_messages')).toHaveText(`Przelew wykonany! ${expectedTransferReceiver} - ${transferAmount},00PLN - ${transferTitle}`);
     });
-    test.only('successeful mobile top-up', async ({ page }) => {
+
+    test('successeful mobile top-up', async ({ page }) => {
         await page.goto('https://demo-bank.vercel.app');
         await page.getByTestId('login-input').fill('tester12');
         await page.getByTestId('password-input').fill('password');
@@ -31,6 +44,4 @@ test.describe('Pulpit tests', () => {
 
         await expect(page.locator('#show_messages')).toHaveText('Doładowanie wykonane! 50,00PLN na numer 500 xxx xxx');
     })
-
-
 });
